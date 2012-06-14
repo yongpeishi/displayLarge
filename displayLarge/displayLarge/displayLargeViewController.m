5335f2e026fa9b3d32d15ff7e6b39e7273a51ce4
yongpeishi <pjong@thoughtworks.com>
2012-06-15 00:09:26 +1000

    have a scrollview with a textfield. 'Return' key hide the keyboard

--- displayLarge/displayLarge/displayLargeViewController.m
+++ displayLarge/displayLarge/displayLargeViewController.m
@@ -8,11 +8,56 @@
 
 #import "displayLargeViewController.h"
 
-@interface displayLargeViewController ()
+@interface displayLargeViewController () <UITextFieldDelegate>
 
 @end
 
-@implementation displayLargeViewController
+@implementation displayLargeViewController{
+    UIScrollView *_scrollView;
+    UITextField *_textField;
+}
+
+- (void)loadView
+{
+    CGRect viewFrame = self.navigationController.view.frame;
+    UIView *view = [[UIView alloc] initWithFrame:viewFrame];
+    
+    CGRect screenRect = [[UIScreen mainScreen] bounds];
+    CGFloat landscapeScreenWidth = screenRect.size.height;
+    CGFloat landscapeScreenHeight = screenRect.size.width;
+    
+    _scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, landscapeScreenWidth, landscapeScreenHeight)];
+    _scrollView.backgroundColor = [UIColor yellowColor];
+    
+    CGFloat marginSide = 25;
+    CGFloat marginTop = 90;
+    CGFloat textFieldWidth = landscapeScreenWidth - (2*marginSide);
+    CGFloat textFieldHeight = 110;
+    _textField = [[UITextField alloc] initWithFrame:CGRectMake(marginSide, 
+                                                              marginTop, 
+                                                              textFieldWidth, 
+                                                              textFieldHeight
+                                                              )];
+    _textField.clearsOnBeginEditing = YES;
+    _textField.clearButtonMode = UITextFieldViewModeWhileEditing;
+    _textField.backgroundColor = [UIColor lightGrayColor];
+    _textField.minimumFontSize = 10;
+    _textField.font = [UIFont systemFontOfSize:100];
+    _textField.adjustsFontSizeToFitWidth = YES;
+    _textField.returnKeyType = UIReturnKeyDone;
+    _textField.textAlignment = UITextAlignmentCenter;
+    _textField.delegate = self;
+    
+    [_scrollView addSubview:_textField];
+    [view addSubview:_scrollView];
+    
+    self.view = view;
+}
+
+-(BOOL) textFieldShouldReturn:(UITextField *)textField{
+    [textField resignFirstResponder];
+    return YES;
+}
 
 - (void)viewDidLoad
 {
@@ -28,7 +73,7 @@
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+    return (interfaceOrientation != UIInterfaceOrientationPortrait);
 }
 
 @end
