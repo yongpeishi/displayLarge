ee953a537dd06a5ce3f4351920b9b5c7004e7521
yongpeishi <pjong@thoughtworks.com>
2012-06-27 04:55:03 +1000

    scroll the view when keyboard hide the textbox

--- displayLarge/displayLarge/displayLargeViewController.m
+++ displayLarge/displayLarge/displayLargeViewController.m
@@ -59,9 +59,64 @@
     return YES;
 }
 
+- (void)registerForKeyboardNotifications
+{
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWasShown:)
+                                                 name:UIKeyboardDidShowNotification object:nil];
+
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillBeHidden:)
+                                                 name:UIKeyboardWillHideNotification object:nil];
+
+}
+
+// Called when the UIKeyboardDidShowNotification is sent.
+- (void)keyboardWasShown:(NSNotification*)notification
+{
+    NSDictionary* info = [notification userInfo];
+    CGSize kbSize = [[info objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+    CGFloat landscapeKeyboardHeight = kbSize.width;
+
+    UIEdgeInsets contentInsets = UIEdgeInsetsMake(0.0, 0.0, 0.0, landscapeKeyboardHeight);
+    _scrollView.contentInset = contentInsets;
+    _scrollView.scrollIndicatorInsets = contentInsets;
+
+    // If active text field is hidden by keyboard, scroll it so it's visible
+    // Your application might not need or want this behavior.
+    CGRect viewFrame = self.view.frame;
+    CGRect landscapeViewFrameWithKeyboard = CGRectMake(viewFrame.origin.y,
+                                           viewFrame.origin.x,
+                                           viewFrame.size.height,
+                                           viewFrame.size.width-landscapeKeyboardHeight);
+
+    CGFloat textFieldLowerLeftY = _textField.frame.origin.y+_textField.frame.size.height;
+    CGFloat textFieldMarginBottom = 35;
+
+    CGPoint lowerLeftPointToBeShown = CGPointMake(_textField.frame.origin.x,
+                                       textFieldLowerLeftY + textFieldMarginBottom);
+
+    BOOL shouldScroll = !CGRectContainsPoint(landscapeViewFrameWithKeyboard, lowerLeftPointToBeShown);
+
+    if (shouldScroll) {
+        CGPoint scrollPoint = CGPointMake(0.0,
+                                          lowerLeftPointToBeShown.y - landscapeKeyboardHeight);
+        [_scrollView setContentOffset:scrollPoint animated:YES];
+    }
+}
+
+// Called when the UIKeyboardWillHideNotification is sent
+- (void)keyboardWillBeHidden:(NSNotification*)notification
+{
+    UIEdgeInsets contentInsets = UIEdgeInsetsZero;
+    _scrollView.contentInset = contentInsets;
+    _scrollView.scrollIndicatorInsets = contentInsets;
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    [self registerForKeyboardNotifications];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
